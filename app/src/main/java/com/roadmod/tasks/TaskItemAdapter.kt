ff052ea00b6069f898540c0a19171168c3b3ccb2
mikhailshauchenka <mike1shef@gmail.com>
2023-03-01 23:57:17 +0100

    Add checkbox and grid

--- app/src/main/java/com/roadmod/tasks/TaskItemAdapter.kt
+++ app/src/main/java/com/roadmod/tasks/TaskItemAdapter.kt
@@ -2,8 +2,11 @@ package com.roadmod.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 
 class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>() {
@@ -22,17 +25,21 @@ class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>
         holder.bind(item)
     }
 
-    class TaskItemViewHolder (val rootView: TextView)
+    class TaskItemViewHolder (val rootView: CardView)
         :RecyclerView.ViewHolder(rootView){
 
+        val taskName = rootView.findViewById<TextView>(R.id.task_name)
+        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+
         fun bind (item: Task) {
-            rootView.text = item.taskName
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
         }
             companion object {
                 fun inflateFrom(parent: ViewGroup) : TaskItemViewHolder {
                     val layoutInflater = LayoutInflater.from(parent.context)
                     val view = layoutInflater
-                        .inflate(R.layout.task_item, parent, false) as TextView
+                        .inflate(R.layout.task_item, parent, false) as CardView
                     return TaskItemViewHolder(view)
                 }
             }
